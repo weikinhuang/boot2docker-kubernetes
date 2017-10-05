462f03a42766c328366c4d051ca60003177030e4
Wei Kin Huang <weikin.huang04@gmail.com>
2017-10-05 12:14:49 -0400

    Simplify bash scripts

--- k8s-master/container/usr/local/bin/docker-entrypoint.sh
+++ k8s-master/container/usr/local/bin/docker-entrypoint.sh
@@ -2,33 +2,56 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-set -x
+function b2d-k8s::is::master() {
+    [[ -n ${K8S_MASTER_NODE:-} ]]
+}
 
-if [[ -n ${K8S_MASTER_NODE:-} ]] && echo "$@" | grep -q '/sbin/init'; then
-    # setup systemd cgroup hierarchy at /sys/fs/cgroup/systemd
-    docker-host.sh run --rm --privileged -v /:/host "${SYSTEMD_SETUP_IMAGE}" setup || true
-else
-    # wait for master node to finish `setup`
-    sleep 5
-fi
+function b2d-k8s::is::entrypoint() {
+    echo "$@" | grep -q '/sbin/init'
+}
 
-if [[ ! -e /.node-setup ]]; then
-    if [[ -n ${K8S_MASTER_NODE:-} ]]; then
-        # set up master node configs
-        rm -f /mnt/share/data/kubeconfig.node
-        . setup-master-node.sh
+function bd2-k8s::setup::master-node() {
+    rm -f /mnt/share/data/kubeconfig.node
+    systemctl enable bootkube.service
+}
+
+function bd2-k8s::setup::worker-node() {
+    systemctl enable kubeconfig.path
+}
+
+function bd2-k8s::setup::node() {
+    if b2d-k8s::is::master; then
+        # setup systemd cgroup hierarchy at /sys/fs/cgroup/systemd
+        docker-host.sh run --rm --privileged -v /:/host "${SYSTEMD_SETUP_IMAGE}" setup || true
     else
-        # set up worker node configs
-        . setup-worker-node.sh
+        # wait for master node to finish `setup`
+        sleep 5
     fi
 
-    env | grep '^BOOTKUBE_' > /etc/systemd/system/bootkube.env || true
-    env | grep '^HYPERKUBE_' > /etc/systemd/system/hyperkube.env || true
-    env | grep '^KUBELET_' > /etc/systemd/system/kubelet.env || true
+    if [[ ! -e /.node-setup ]]; then
+        if b2d-k8s::is::master; then
+            # set up master node configs
+            bd2-k8s::setup::master-node
+        else
+            # set up worker node configs
+            bd2-k8s::setup::worker-node
+        fi
 
-    echo "K8S_MASTER_NODE=${K8S_MASTER_NODE:-}" >> /etc/systemd/system/kubelet.env || true
+        env | grep '^BOOTKUBE_' > /etc/systemd/system/bootkube.env || true
+        env | grep '^HYPERKUBE_' > /etc/systemd/system/hyperkube.env || true
+        env | grep '^KUBELET_' > /etc/systemd/system/kubelet.env || true
 
-    touch /.node-setup
+        echo "K8S_MASTER_NODE=${K8S_MASTER_NODE:-}" >> /etc/systemd/system/kubelet.env || true
+
+        touch /.node-setup
+    fi
+}
+
+
+if b2d-k8s::is::entrypoint "$@"; then
+    set -x
+    bd2-k8s::setup::node "$@"
+    set +x
 fi
 
 # call the real command
