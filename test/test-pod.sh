402ad32a644c0ee669abdf08b24917ffd8311b8c
Wei Kin Huang <weikin.huang04@gmail.com>
2017-10-10 23:03:58 -0400

    Add a pod and service to test cluster

---
+++ test/test-pod.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+function create-test-pod() {
+    docker-compose exec master bash -c "env KUBECONFIG=/root/assets/auth/kubeconfig kubectl apply -f /data/etest/echoserver.yaml"
+}
+
+function get-pods() {
+    docker-compose exec master bash -c "env KUBECONFIG=/root/assets/auth/kubeconfig kubectl get pods -o wide"
+}
+
+function is-pod-ready() {
+    docker-compose exec master bash -c "env KUBECONFIG=/root/assets/auth/kubeconfig kubectl get pods -l app=echoheaders" | grep -q '\<Running\>'
+}
+
+COUNTER=0
+EXIT_STATUS=1
+while [  $COUNTER -lt 30 ]; do
+    get-pods
+    if is-pod-ready; then
+        EXIT_STATUS=0
+        break
+    fi
+    let COUNTER=COUNTER+1
+    sleep 10
+done
+
+if [[ ${EXIT_STATUS} == 1 ]]; then
+    get-pods
+    echo "There no pods ready!"
+    exit 1
+fi
+
+curl --fail -SL --connect-timeout 10 --max-time 15 -v -i http://127.0.0.1:30001
